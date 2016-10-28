4594965c9418fd9f398052ac95bc66cee5c52f19
bruno Fischlin <bruno.fischlin@stud.hslu.ch>
2016-10-28 15:55:20 +0100

    Included TEsseract

--- JavaApplication3/src/abc/TestWebCam.java
+++ JavaApplication3/src/abc/TestWebCam.java
@@ -28,6 +28,9 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.opencv.core.Core;
 import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.core.CvType;
@@ -87,6 +90,7 @@ public class TestWebCam extends JPanel implements ActionListener {
         //CascadeClassifier faceDetector=new CascadeClassifier("C:\\opencv\\sources\\data\\lbpcascades\\lbpcascade_profileface.xml");
         TestWebCam toc = new TestWebCam();
 
+
         frame.add(toc);;
         frame.setVisible(true);
         Mat webcam_image = new Mat();
@@ -97,18 +101,14 @@ public class TestWebCam extends JPanel implements ActionListener {
         } catch (Exception xx) {
             xx.printStackTrace();
         }
-        if (capture.open(1)) { //open(0) opens your Laptop webcam, open(1) opens USB attached Webcam
+        if (capture.open(0)) { //open(0) opens your Laptop webcam, open(1) opens USB attached Webcam
             while (true) {
                 capture.read(webcam_image);
                 if (!webcam_image.empty()) {
-
-                    
+                    //templateMatchingWithRed(webcam_image, thisPath);
                     String thisPath = TestWebCam.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
                     thisPath = thisPath.substring(1, thisPath.length());
                     thisPath = thisPath.replace("/", "\\");
-
-                    //templateMatchingWithRed(webcam_image, thisPath);
-                    
                     //Mat test = detectRedRects(webcam_image);
                     Mat hsv_image = new Mat();
                     Imgproc.cvtColor(webcam_image, hsv_image, Imgproc.COLOR_BGR2HSV);
@@ -116,18 +116,22 @@ public class TestWebCam extends JPanel implements ActionListener {
                     Mat lower_red_hue_range = new Mat();
                     Mat upper_red_hue_range = new Mat();
 
-                    Core.inRange(hsv_image, new Scalar(0,100,100), new Scalar(1, 227, 255), lower_red_hue_range);
-                    Core.inRange(hsv_image, new Scalar(160, 100, 100), new Scalar(179, 227, 255), upper_red_hue_range);
+                    /*Core.inRange(hsv_image, new Scalar(0,100,100), new Scalar(1, 227, 255), lower_red_hue_range);
+                    Core.inRange(hsv_image, new Scalar(160, 100, 100), new Scalar(179, 227, 255), upper_red_hue_range);*/
 
+                    Core.inRange(hsv_image, new Scalar(0,100,100), new Scalar(10, 255, 255), lower_red_hue_range);
+                    Core.inRange(hsv_image, new Scalar(160, 100, 100), new Scalar(179, 255, 255), upper_red_hue_range);
 
                     Mat red_hue_image = new Mat();
                     Core.addWeighted(lower_red_hue_range, 1.0, upper_red_hue_range, 1.0, 0.0, red_hue_image);
+
                     Imgproc.GaussianBlur(red_hue_image, red_hue_image, new Size(9, 9), 2, 2);
 
                     List<MatOfPoint> contours = new ArrayList<>();
                     Mat hirarchy = new Mat();
-                    
+
                     Imgproc.findContours(red_hue_image, contours, new Mat(), Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
+
                     MatOfPoint2f approxCurve = new MatOfPoint2f();
                      int x = 1;
                      int barCount = 0;
@@ -137,7 +141,7 @@ public class TestWebCam extends JPanel implements ActionListener {
                          //Convert contours(i) from MatOfPoint to MatOfPoint2f
                          MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray() );
                          //Processing on mMOP2f1 which is in type MatOfPoint2f
-                         double approxDistance = Imgproc.arcLength(contour2f, true)*0.002;
+                         double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
                          Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
 
                          //Convert back to MatOfPoint
@@ -145,7 +149,7 @@ public class TestWebCam extends JPanel implements ActionListener {
 
                          // Get bounding rect of contour
                          Rect rect = Imgproc.boundingRect(points);
-                        if(rect.height > 100 ) {
+                        if(rect.height > 50 ) {
                           // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
                             barCount++; 
                         }
@@ -154,11 +158,22 @@ public class TestWebCam extends JPanel implements ActionListener {
                      
                     if(barCount > 2){
                         System.out.println("Found Bars");
+
+                        ITesseract instance = new Tesseract();
+                        try{
+                            MatToBufImg webcamImageBuff = new MatToBufImg();
+                            webcamImageBuff.setMatrix(webcam_image, ".jpg");
+                            String result = instance.doOCR(webcamImageBuff.getBufferedImage());
+                            System.out.println("found: " + result);
+                        }catch(Exception e){
+                            System.err.println(e.getMessage());
+                        }
+/*
                         Mat[] templates = new Mat[5];
                         for(int count = 0; count < 5; count++){
                             templates[count] = Highgui.imread(thisPath+"abc\\Roman_"+(count+1)+".jpg");
                         }
-
+                        
                         int match_method = Imgproc.TM_SQDIFF;
 
 
@@ -166,7 +181,8 @@ public class TestWebCam extends JPanel implements ActionListener {
                        //Imgproc.matchTemplate(webcam_image, templates[3], result, match_method);
                        // Core.normalize(result, result, 0, 1, Core.NORM_MINMAX, -1, new Mat());
                        int counter = 1;
-                       int newFoundImage = 0;
+
+                       double[] match_Array = new double[5];
                        for(Mat p : templates){
                             // / Create the result matrix
                             int result_cols = webcam_image.cols() - p.cols() + 1;
@@ -185,27 +201,42 @@ public class TestWebCam extends JPanel implements ActionListener {
                             if((double)(mmr_Array.minVal) < (double)(threshold)){
 
                                Core.rectangle(webcam_image, matchLoc_Array, new Point(matchLoc_Array.x + p.cols(),
-                               matchLoc_Array.y + p.rows()), new Scalar(0, 255, 0));
-                               newFoundImage = counter;
+                               matchLoc_Array.y + p.rows()), new Scalar(0, 255, 5));
+                               //newFoundImage = counter;
+                               match_Array[counter-1] = mmr_Array.minVal;
                             }
                             counter++;
                        }
-
+                       for(double d : match_Array){
+                           System.out.print(d + " : ");
+                       }
+                        int newFoundImage = 0;
+                        double highestMatch = match_Array[0];
+                        for( int count  = 0; count < match_Array.length; count++){
+                            System.out.print(" : " + match_Array[count]);
+                            if(match_Array[count] < highestMatch){
+                                highestMatch = match_Array[count];
+                                newFoundImage = count + 1;
+                            }
+                        }
+                        System.out.print("\n");
+                        
                         if(foundImage != newFoundImage){
                             if(newFoundImage != 0){
                                 System.out.println("new found Roman "+ newFoundImage);
                             }
                             foundImage = newFoundImage;
-                        } 
+                        } */
                     }
                     //Imgproc.drawContours(webcam_image, contours, -1, new Scalar(255,255,0));
                     
                     //Mat templ = Highgui.imread(thisPath+"abc\\Roman_3.jpg");
-
-
                     mat2Buf.setMatrix(webcam_image, ".jpg");
                     toc.setimage(mat2Buf.getBufferedImage());
                     toc.repaint();
+
+                    
+
                 } else {  
                   System.out.println("problems with webcam image capture");
                     try{ 
